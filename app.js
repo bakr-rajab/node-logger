1f4bc5ccdaf9db199d4bb7ea0c0466cf5de5492f
bakr-rajab <abobakr.r.bakry@gmail.com>
2022-06-14 00:08:34 +0200

    handel error

--- app.js
+++ app.js
@@ -19,13 +19,16 @@ mongoose.connect(process.env.MONGODB_URI)
   })
 
 app.use(cors())
-app.use(express.static('build'))
+// app.use(express.static('build'))
 app.use(express.json())
 
-app.use(middleware.requestLogger)
+// app.use(middleware.requestLogger)
 app.use('/api/notes', notesRouter)
-
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
+// handle undefind request
+app.all("*", (req, res,next)=> {
+  res.status(404).json({message:"cant find "+ req.originalUrl ,status:"fail"})
+})
 module.exports = app
--- utils/middleware.js
+++ utils/middleware.js
@@ -4,12 +4,12 @@ var client = require('mongoose')
 
 
 const requestLogger =  (request, response, next) => {
-  logger.info(request.method)
-  let p = request.path.split('/')[2]
-  console.log("99999999", p);
-  logger.info(`"PATH: ", ${request.path}`)
-  logger.info("body : ", request.body)
-  logger.info('---')
+  // logger.info(request.method)
+  // let p = request.path.split('/')[2]
+  // console.log("99999999", p);
+  // logger.info(`"PATH: ", ${request.path}`)
+  // logger.info("body : ", request.body)
+  // logger.info('---')
 
   try {
     if(request.method != 'GET'){
@@ -30,20 +30,30 @@ const requestLogger =  (request, response, next) => {
   }
 }
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
+const unknownEndpoint = (request, response, next) => {
+  console.log("55555555555552222255");
+  // response.status(404).send({ error: 'unknown endpoint' })
+  const err=new Error(`cant find >>>> ${request.originalUrl} `)
+  err.statusCode= 404;
+  err.status= 'error1';
+  next(err);
 }
 
 const errorHandler = (error, request, response, next) => {
-  logger.error(error.message)
+  let err={};
+  err.message = error.message;
+  err.statusCode=error.statusCode || 500;
+  err.status=err.status || 'error'
+  logger.error(err)
 
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  }
+  response.status(err.statusCode).json({message:err.message,status:error.status})
+  // if (error.name === 'CastError') {
+  //   return response.status(400).send({ error: 'malformatted id' })
+  // } else if (error.name === 'ValidationError') {
+  //   return response.status(400).json({ error: error.message })
+  // }
 
-  next(error)
+  // next(error)
 }
 module.exports = {
   requestLogger,
